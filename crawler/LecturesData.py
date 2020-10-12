44fad46e5fbb3373acc42a0aa4e2548cfc1e6792
xcli17 <706593160@qq.com>
2020-10-12 14:11:09 +0800

    Changes to be committed: 	new file:   crawler/LecturesData.py 	new file:   crawler/WeChatArticlesInfo.py Author: xcli Datetime: 2020-10-12 14:11

---
+++ crawler/LecturesData.py
@@ -0,0 +1,129 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class LecturesData(object):
+    '''
+    获取讲座的信息
+    '''
+    COLUMNS = ['title', 'reporter', 'time', 'place', 'holder']
+    def __init__(self):
+        self.root_url = "https://kyybgxx.cic.tsinghua.edu.cn/kybg/xsgg/"
+        self.root = requests.get(self.root_url)
+        self.root_bs = BeautifulSoup(self.root.text, 'lxml')
+        self.raw_columns = ['报告题目:', '报告人:', '报告时间:', '报告地点:', '主办单位:', '简介:', '链接:']
+        self.columns = ['title', 'reporter', 'time', 'place', 'holder', 'abstract', 'url']
+        self.col_dict = dict(zip(self.raw_columns, self.columns))
+    
+    def get_lectures_details(self, detail_url:str):
+        """
+        根据传入的讲座详情页url进行解析，提取出指定的列
+        Parameter:
+            detail_url: str, 详情网页的url
+
+        Return:
+            dict
+        """
+        detail_page=requests.get(detail_url)
+        detail_bs = BeautifulSoup(detail_page.text, 'lxml')
+        detail_trs = detail_bs.find_all('tr')
+        detail_tr_dict = {}
+        for tr in detail_trs:
+            tds = tr.find_all('td')
+            if len(tds) <= 0:
+                continue
+            node = tds[0].find('div')
+            if node is not None and node.string is not None:
+                s = node.string.strip()
+                if s in self.raw_columns:
+                    col = self.col_dict[s]
+                    if col == 'abstract':
+                        detail_tr_dict['abstract'] = ' '.join(tds[1].text.split())
+                    else:
+                        detail_tr_dict[col] = tds[1].string
+        detail_tr_dict['url'] = detail_url
+        return detail_tr_dict
+
+    def get_lectures_tag(self, tag_node, page_count:int=-1):
+        """
+        根据传入的讲座tag的node进行解析
+        Parameter:
+            tag_node: bs4.element.Tag, 
+            page_count: int, 读取的页数 
+
+        Return:
+            list of dict
+        """
+        tag_data_list=[]
+        tag = tag_node.string
+        tag_url = tag_node['href']
+        tag_url = self.root_url + tag_url
+        tag_page = requests.get(tag_url)
+        count = 0
+        while True:
+            tag_bs = BeautifulSoup(tag_page.text, 'lxml')
+            lec_trs = tag_bs.find_all('tr')
+            ccc=0
+            for tr in lec_trs:
+                # if tr.get('bgcolor', None) is None or tr.get('class', None) is not None:
+                #     continue
+                tds = tr.find_all('td')
+                if tds is None or len(tds) < 2:
+                    continue
+                a = tds[1].find('a')
+                if a is None:
+                    continue
+                detail_url = a.get('href')
+                if 'detail' not in detail_url:
+                    continue
+                detail_url = self.root_url + detail_url
+                print(detail_url)
+                tag_data_list.append(self.get_lectures_details(detail_url))
+                # 测试用代码，限制读取条数减少用时
+                # if len(tag_data_list) > 4:
+                #     print(pd.DataFrame(tag_data_list))
+                #     break
+            nextpage_node=tag_bs.find('a', string=re.compile('.*下.*页.*'))
+            if nextpage_node is None:
+                break
+            nextpage_url = self.root_url + nextpage_node['href']
+            tag_page = requests.get(nextpage_url)
+            count += 1
+            if page_count > 0 and count >= page_count:
+                break
+        return tag_data_list
+
+    def get_lectures_data(self, tag_count:int=-1, page_count:int=-1):
+        """
+        读取分tag的讲座信息
+        Parameter:
+            tag_count: int, 读取的tag数 
+            page_count: int, 读取的页数 
+
+        Return:
+            dict of pd.DataFrame
+        """
+        root_trs = self.root_bs.find_all(name='td', id=re.compile("category[0-9][0-9][0-9][0-9].+"))
+        data_dict = {}
+        count = 0
+        for tag_root in root_trs:
+            tag_data_list = []
+            tag_node = tag_root.find('a')
+            if tag_node is None:
+                break
+            tag = tag_node.string
+            data_dict[tag] = []
+            data_dict[tag] = pd.DataFrame.from_records(self.get_lectures_tag(tag_node, page_count))
+            count += 1
+            if count >= tag_count:
+                break
+        return data_dict
+
+
+if __name__ == "__main__":
+    #约需要5min
+    lecturesdata = LecturesData()
+    result = lecturesdata.get_lectures_data(tag_count=2, page_count=2)
+    for key in result:
+        print(result[key].head())
---
+++ crawler/WeChatArticlesInfo.py
@@ -0,0 +1,134 @@
+import requests
+import time
+import pandas as pd
+
+class WeChatArticlesInfo(object):
+    """
+    获取微信公众号的推文信息
+    """
+    COLUMNS = ['aid', 'title', 'cover_url', 'abstract', 'url', 'time']
+    INFO_KEY = "app_msg_list"
+    COUNT_KEY = "app_msg_cnt"
+    def __init__(self, cookie:str, token:str):
+        """
+        Parameter:
+            cookie: str
+            token: str
+        """
+        self.sess = requests.session()
+        self.search_url = "https://mp.weixin.qq.com/cgi-bin/searchbiz"
+        self.appmsg_url = "https://mp.weixin.qq.com/cgi-bin/appmsg"
+        self.headers = {
+            "Cookie": cookie,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36"
+        }
+        self.params = {
+            "lang": "zh_CN",
+            "f": "json",
+            "token": token
+        }
+        self.fakeid = {}
+
+    def get_basic_info(self, name:str, begin:int=0, count:int=5):
+        """
+        查找公众号基础信息，主要用于获取fakeid进行下一步的获取
+        Parameter:
+            name: str, 公众号名称，尽可能准确
+            begin: int, 起始页数
+            count: int, 数量，1-5
+
+        Return:
+            list
+        """
+        params = {
+            "query": name,
+            "count": str(count),
+            "action": "search_biz",
+            "ajax": "1",
+            "begin": str(begin)
+        }
+        self.params.update(params)
+        # 公众号名称一定要准确，否则排序上会出问题
+        result = self.sess.get(self.search_url,
+                                 headers=self.headers,
+                                 params=self.params)
+        return result.json()["list"]
+
+    def __get_articles_info(self, name:str, begin:int, count:int=5):
+        """
+        获取公众号文章的信息，包含url(key: link)可用于后续爬取
+        Parameters:
+            name: str, 公众号名称，尽可能准确
+            begin: int, 起始页数
+            count: int, 数量，1-5
+        Returns
+            json
+            important keys:
+            "app_msg_list": 公众号文章信息
+            "app_msg_cnt": 公众号文章总数
+        """
+        fakeid = self.fakeid.get(name, None)
+        if fakeid is None:
+            self.fakeid[name] = self.get_basic_info(name)[0]["fakeid"]
+            fakeid = self.fakeid[name]
+        params = {
+            "fakeid": fakeid,
+            "query": "",
+            "begin": str(begin),
+            "count": str(count),
+            "type": "9",
+            "action": "list_ex"
+        }
+        self.params.update(params)
+        data = self.sess.get(self.appmsg_url, headers=self.headers, params=self.params)
+        return data.json()
+    
+    def get_articles_info_step(self, name:str, begin:int=0, count:int=5):
+        """
+        查找并提取文章的信息
+        Parameter:
+            name: str, 公众号名称，尽可能准确
+            begin: int, 起始页数
+            count: int, 数量，1-5
+
+        Return:
+            list
+        """
+        data = self.__get_articles_info(name=name, begin=begin, count=count)[WeChatArticlesInfo.INFO_KEY]
+        map_dict = {'aid': 'aid',
+                    'title': 'title', 
+                    'cover_url': 'cover',
+                    'abstract': 'digest',
+                    'url': 'link', 
+                    'time': 'update_time'}
+        data = [{key: x[map_dict[key]] for key in map_dict} for x in data]
+        return data
+    
+    def get_articles_info(self, name:str, begin:int=0, count:int=15):
+        """
+        查找并提取文章的信息
+        Parameter:
+            name: str, 公众号名称，尽可能准确
+            begin: int, 起始页数
+            count: int, 数量
+            endtimestamp: int，终止的timestamp
+
+        Return:
+            pd.DataFrame
+        """
+        nsteps = int(count / 5) + 1
+        step = 0
+        data = []
+        while 5 * step < count:
+            data.extend(self.get_articles_info_step(name, 5*step, 5))
+            step = step + 1
+            #time.sleep(0.01)
+        return pd.DataFrame.from_records(data)
+
+if __name__ == "__main__":
+    cookie = input("Please input cookie:\n")
+    token = input("Pleanse input token:\n")
+    print("\n")
+    wcai = WeChatArticlesInfo(cookie=cookie.strip(), token=token.strip())
+    result = wcai.get_articles_info("清华大学", begin=0, count=20)
+    print(result.head(10))
