ef1241016513464e34d159487291387d2b6ed2f3
xcli17 <706593160@qq.com>
2020-10-27 23:39:44 +0800

    解决了异步操作的问题，目前订单模块核心功能getOrderData已经可用 - xcli
    Note: 所有的异步操作的后续操作均应在上级操作的then中，且均需要return结果

--- 订单模块/yunkaifa/cloudfunctions/getOrderData/index.js
+++ 订单模块/yunkaifa/cloudfunctions/getOrderData/index.js
@@ -11,34 +11,83 @@ const orderTableName = "Order";
 exports.main = async (event, context) => {
   //state, 0为未接单，1为已接单，2为已完成
   var {uid, state, start, count} = event;
-  state = checkNumber(state, 0);
-  start = checkNumber(start, 0);
-  count = checkNumber(count, 20);
   if((state == null) || (start == null) || (count == null)){
     return null;
   }
-  return await db.collection(orderTableName).where(conds)
+  return await db.collection("UserInfo")
+  .where({
+    uid: _.eq(uid)
+  })
+  .get()
+  .then((res)=>{
+    var userinfo_query = res.data;
+    //若同一个uid获取到的条数大于1则在控制台报错，需要进行处理
+    //TODO: 未来可以写入日志
+    if(userinfo_query.length>1){
+      console.log("同一uid对应多条用户记录，请检查数据库！\n"+"记录为："+userinfo.toString()+"\n");
+      // throw new Error("同一uid对应多条用户记录，请检查数据库！\n"+"记录为："+userinfo.toString()+"\n");
+    }
+    var role = userinfo_query[0].role;
+    var conds = {
+      state: _.eq(state)
+    }; 
+    //查询时，志愿者和用户仅能看到自己相关的订单，管理员可以看到所有订单
+    switch(role){
+      case "志愿者":
+        if(state == 0){
+          return queryVolunteer0(uid, state, start, count, conds);
+        }
+        else{
+          conds.auid = _.eq(uid);
+        }
+        break;
+      case "用户":
+        conds.puid = _.eq(uid);
+        break;
+      case "管理员":
+        break;
+      default:
+        throw new Error("Illegal Role Type: "+role+"\n");
+    };
+    return queryOrderData(conds, start, count);
+  })
+}
+
+// function checkNumber(val, val_default){
+//   if(!(val instanceof Number)){
+//     if(!(val instanceof String)){
+//       val = val_default;
+//     }
+//     else{
+//       val = Number(val);
+//     }
+//   }
+//   return val;
+// }
+
+function queryVolunteer0(uid, state, start, count, conds){
+  return db.collection("VolunteerInfo")
+    .where({
+      uid: _.eq(uid)
+    })
+    .get()
+    .then(res=>{
+      var subjects = [];
+      for(let vinfo in res.data){
+        subjects.push(vinfo.sid);
+        conds.sid = _.in(subjects);
+      }
+      return queryOrderData(conds, start, count);
+    })
+}
+
+function queryOrderData(conds, start, count){
+  return db.collection(orderTableName)
+  .where(conds)
   .skip(start)
   .limit(count)
   .get()
   .then(res=>{
     return res;
   })
-  .catch(err=>{
-    console.log(err);
-    return [];
-  })
 }
-
-function checkNumber(val, val_default){
-  if(!(val instanceof Number)){
-    if(!(val instanceof String)){
-      val = val_default;
-    }
-    else{
-      val = Number(val);
-    }
-  }
-  return val;
-}
-//查询时，志愿者和用户仅能看到自己相关的订单，管理员可以看到所有订单
--- 订单模块/yunkaifa/miniprogram/pages/dayi/dayi.js
+++ 订单模块/yunkaifa/miniprogram/pages/dayi/dayi.js
@@ -9,79 +9,6 @@ Page({
     start: 0
   },
 
-  // getUserInfo: function(uid){
-  //   const db = wx.cloud.database();
-  //   const _ = db.command;
-  //   return wx.cloud.callFunction({
-  //     name: "joinSQL",
-  //     data: {
-  //       tables: ["UserInfo", "VolunteerInfo"],
-  //       on: [[], "uid"],
-  //       conds: {
-  //         uid: _.eq(uid),
-  //       }
-  //     }
-  //   })
-  //   .then(res=>{
-  //     return res;
-  //   });
-  // },
-
-  getOrderData: function(uid, state, start, count){
-    const db = wx.cloud.database();
-    const _ = db.command;
-    // var userinfo_query = this.getUserInfo(uid);
-    var userinfo_query = wx.cloud.callFunction({
-      name: "getUserInfo",
-      data: {
-        uid: uid
-      }
-    });
-    console.log("********");
-    console.log(userinfo_query);
-    //若同一个uid获取到的条数大于1则在控制台报错，需要进行处理
-    //TODO: 未来可以写入日志
-    // if(userinfo_query.length>1){
-    //   console.log("同一uid对应多条用户记录，请检查数据库！\n"+"记录为："+userinfo.toString()+"\n");
-    //   // throw new Error("同一uid对应多条用户记录，请检查数据库！\n"+"记录为："+userinfo.toString()+"\n");
-    // }
-    var role = userinfo_query.role;
-    var conds = {
-      state: _.eq(state)
-    }; 
-    switch(role){
-      case "志愿者":
-        if(state == 0){
-          var subjects = [];
-          for(let vinfo in query_result[1].data){
-            subjects.push(vinfo.sid);
-          }
-          conds.sid = _.in(subjects);
-        }
-        else{
-          conds.auid = _.eq(uid);
-        }
-        break;
-      case "用户":
-        conds.puid = _.eq(uid);
-        break;
-      case "管理员":
-        break;
-      default:
-        throw new Error("Illegal Role Type: "+role+"\n");
-  }
-
-    return wx.cloud.callFunction({
-      name:"getOrderData",
-      data:{
-        uid: uid,
-        state: state,
-        start: start,
-        count: count,
-      }
-    })
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -91,14 +18,24 @@ Page({
       title: '正在查询订单',
     })
     //云函数查询订单队列数据
-    this.getOrderData(0, 0, 0, 10)
-    .then(res=>{
-    this.setData({
-      orderArr:res.result.data
+    wx.cloud.callFunction({
+      name: "getOrderData",
+      data: {
+        //TODO: uid之后需要更换为微信号关联的的某个id
+        uid: 0,
+        state: 1,
+        start: 0,
+        count: 10
+      }
     })
-    console.log(this.data.orderArr);
-    //关闭加载信息
-    wx.hideLoading()
+    .then(res=>{
+      console.log(res);
+      this.setData({
+        orderArr:res.result.data
+      })
+      console.log(this.data.orderArr);
+      //关闭加载信息
+      wx.hideLoading()
     })
   },
 
